38e22846416cec9b376dfbd6433d08bfd30eed67
Phil Housley <philip_housley@scee.net>
2017-02-06 17:22:01 +0000

    Untested: Make cancelable

--- fetchurls/fetcher.go
+++ fetchurls/fetcher.go
@@ -2,14 +2,33 @@ package fetchurls
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
+
+var (
+	// ErrCancelled is the fetcher was shut down
+	ErrCancelled = errors.New("cancelled")
+	// ErrFailed is the URL could not be fetched
+	ErrFailed = errors.New("failed_to_read")
+	// ErrUnsupported is the URL scheme is not supported
+	ErrUnsupported = errors.New("unsupported")
+)
+
+// Fetcher fetches URLs about once a minute and sends you the data if it has changed
+type Fetcher struct {
+	client *http.Client
+	out    chan FetchedData // return new data
+	in     chan []string    // accept new url sets
+	work   chan toFetch     // push work to workers
+}
 
 // FetchedData encapsulates a newly found data from a URL, or an error in accessing a URL.
 type FetchedData struct {
@@ -28,6 +47,7 @@ type fetchState struct {
 
 // toFetch is the instruction format for workers
 type toFetch struct {
+	ctx  context.Context
 	url  string
 	etag string
 }
@@ -39,14 +59,6 @@ type fromFetch struct {
 	time int64
 	data []byte
 	err  error
-}
-
-// Fetcher fetches URLs about once a minute and sends you the data if it has changed
-type Fetcher struct {
-	client *http.Client
-	out    chan FetchedData // return new data
-	in     chan []string    // accept new url sets
-	work   chan toFetch     // push work to workers
 }
 
 // New creates a new Fetcher
@@ -64,17 +76,17 @@ func (f *Fetcher) Start(ctx context.Context) {
 
 	resultCh := make(chan fromFetch)
 
+	// kick off fixed number of workers
 	for i := 0; i < 2; i++ {
 		go func() {
+			// forever: either do a fetch or quit
 			for {
 				select {
 				case work := <-f.work:
-					bytes, etag, err := doFetch(f.client, work.url, work.etag)
-					if err != nil {
-						resultCh <- fromFetch{url: work.url, err: err}
-						continue
-					}
-					resultCh <- fromFetch{url: work.url, etag: etag, time: time.Now().Unix(), data: bytes}
+					res := doFetch(work.ctx, f.client, work.url, work.etag)
+					res.url = work.url
+					res.time = time.Now().Unix()
+					resultCh <- res
 				case <-ctx.Done():
 					break
 				}
@@ -90,6 +102,7 @@ func (f *Fetcher) Start(ctx context.Context) {
 		for {
 			select {
 			case urls := <-f.in:
+				log.Debugf("new fetch list: %v", urls)
 				newData := map[string]*fetchState{}
 				for _, url := range urls {
 					if existing, ok := data[url]; ok {
@@ -111,7 +124,7 @@ func (f *Fetcher) Start(ctx context.Context) {
 			case result := <-resultCh:
 				if state, ok := data[result.url]; ok {
 					if result.err != nil {
-						state.time = time.Now().Unix()
+						state.time = result.time
 						f.out <- FetchedData{URL: result.url, Time: state.time, Err: result.err}
 					} else if state.etag == result.etag {
 						state.time = result.time
@@ -123,9 +136,9 @@ func (f *Fetcher) Start(ctx context.Context) {
 				} else {
 					// this is no longer an interesting URL
 				}
-				f.tryDispatch(data)
+				f.tryDispatch(ctx, data)
 			case <-ticker.C:
-				f.tryDispatch(data)
+				f.tryDispatch(ctx, data)
 			case <-ctx.Done():
 				ticker.Stop()
 				break loop
@@ -134,11 +147,14 @@ func (f *Fetcher) Start(ctx context.Context) {
 	}()
 }
 
-func (f *Fetcher) tryDispatch(data map[string]*fetchState) {
+// tryDispatch starts as many fetches as possible out of those due to be run
+// The number dispatched is limited by how many workers are currently waiting.
+func (f *Fetcher) tryDispatch(ctx context.Context, data map[string]*fetchState) {
+	// TODO - there's no check against starvation of some URLs
+	cutoff := time.Now().Unix() - 60
 	for url, state := range data {
-		cutoff := time.Now().Unix() - 60
 		if state.time >= 0 && state.time <= cutoff {
-			instruction := toFetch{url: url, etag: state.etag}
+			instruction := toFetch{ctx: ctx, url: url, etag: state.etag}
 			select {
 			case f.work <- instruction:
 				// now in flight
@@ -156,60 +172,97 @@ func (f *Fetcher) SetURLs(urls []string) {
 	f.in <- urls
 }
 
-func doFetch(client *http.Client, urlString, etag string) ([]byte, string, error) {
+// doFetch does the fetch, blocking until complete or cancelled
+func doFetch(ctx context.Context, client *http.Client, urlString, etag string) fromFetch {
 	u, err := url.Parse(urlString)
 	if err != nil {
-		return nil, "", err
+		return fromFetch{err: err}
 	}
 
+	// TODO - remove this repetition
 	if u.Scheme == "http" || u.Scheme == "https" {
-		req, _ := http.NewRequest("GET", urlString, nil)
-		if etag != "" {
-			req.Header.Add("If-None-Match", etag)
+		resCh := make(chan fromFetch)
+		go doHTTPFetch(client, u, urlString, etag, resCh)
+		select {
+		case res := <-resCh:
+			close(resCh)
+			return res
+		case <-ctx.Done():
+			close(resCh)
+			return fromFetch{err: ErrCancelled}
 		}
-
-		resp, err := client.Do(req)
-		if err != nil {
-			return nil, "", fmt.Errorf("failed to fetch: %s", u)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode == 304 {
-			return nil, etag, nil
-		}
-		if resp.StatusCode != 200 {
-			return nil, "", fmt.Errorf("failed to read: %s", u)
-		}
-
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, "", fmt.Errorf("failed to read: %s", u)
-		}
-
-		newEtag := resp.Header.Get("ETag")
-		if newEtag == "" {
-			// server not supplied anything, so must assume changed
-			newEtag = strconv.FormatInt(time.Now().Unix(), 10)
-		}
-
-		return b, newEtag, nil
 	} else if u.Scheme == "file" {
-		fileinfo, err := os.Stat(u.Path)
-		if err != nil {
-			return nil, "", err
+		resCh := make(chan fromFetch)
+		go doFileFetch(u, urlString, etag, resCh)
+		select {
+		case res := <-resCh:
+			close(resCh)
+			return res
+		case <-ctx.Done():
+			close(resCh)
+			return fromFetch{err: ErrCancelled}
 		}
-
-		timestamp := strconv.FormatInt(fileinfo.ModTime().Unix(), 10)
-		if timestamp == etag {
-			return nil, etag, nil
-		}
-
-		b, err := ioutil.ReadFile(u.Path)
-		if err != nil {
-			return nil, "", fmt.Errorf("failed to read: %s", u)
-		}
-		return b, timestamp, nil
 	} else {
-		return nil, "", fmt.Errorf("unsupported scheme: %s", u.Scheme)
+		return fromFetch{err: ErrUnsupported}
 	}
+}
+
+func doHTTPFetch(client *http.Client, u *url.URL, urlString, etag string, resCh chan fromFetch) {
+	req, _ := http.NewRequest("GET", urlString, nil)
+	if etag != "" {
+		req.Header.Add("If-None-Match", etag)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		resCh <- fromFetch{err: ErrFailed}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 304 {
+		resCh <- fromFetch{etag: etag}
+		return
+	}
+	if resp.StatusCode != 200 {
+		resCh <- fromFetch{err: ErrFailed}
+		return
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		resCh <- fromFetch{err: ErrFailed}
+		return
+	}
+
+	newEtag := resp.Header.Get("ETag")
+	if newEtag == "" {
+		// server not supplied anything, so must assume changed
+		newEtag = strconv.FormatInt(time.Now().Unix(), 10)
+	}
+
+	resCh <- fromFetch{data: b, etag: newEtag}
+}
+
+func doFileFetch(u *url.URL, urlString, etag string, resCh chan fromFetch) {
+	fileinfo, err := os.Stat(u.Path)
+	if err != nil {
+		resCh <- fromFetch{err: err}
+		return
+	}
+
+	timestamp := strconv.FormatInt(fileinfo.ModTime().Unix(), 10)
+	if timestamp == etag {
+		resCh <- fromFetch{etag: etag}
+		return
+	}
+
+	b, err := ioutil.ReadFile(u.Path)
+	if err != nil {
+		resCh <- fromFetch{err: ErrFailed}
+		return
+	}
+
+	resCh <- fromFetch{data: b, etag: timestamp}
+	return
 }
